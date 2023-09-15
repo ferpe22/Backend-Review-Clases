142141ea6573fa962ffe66ee2640c1b817d5acf7
Fernando Pereira <ferpereira22@gmail.com>
2023-09-15 14:46:48 -0300

    more changes

--- clase23/server.js
+++ clase23/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const dictionaryRouter = require('./routers/dictionaryRouter')
+const petsRouter = require('./routers/petRouter')
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     return res.json({
@@ -11,6 +15,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/dictionary', dictionaryRouter)
+app.use('/api/pets', petsRouter)
 
 const PORT = 8080
 
