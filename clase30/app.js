f35aa886f921c114054bccbe524d031ed399e7ed
Fernando Pereira <ferpereira22@gmail.com>
2023-10-13 20:30:41 -0300

    clase 30

--- clase30/app.js
+++ clase30/app.js
@@ -1,44 +1,58 @@
-//import express from 'express';
-//import nodemailer from 'nodemailer';
+import express from 'express';
+import nodemailer from 'nodemailer';
 
-const express = require('express')
-const nodemailer = require('nodemailer')
+// const express = require('express')
+// const nodemailer = require('nodemailer')
 
 const app = express()
 
 // app.get('/mail', (req, res) => {
-//   return res.send('Mail enviado')
+//   res.send('Mail enviado')
 // })
 
-const transport = nodemailer.createTransport({
-  host: 'gmail',
+app.listen(3000, () => {
+  console.log('Servidor corriendo en el puerto 3000')
+})
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
   port: 587,
   //secure: true,
   auth: {
     user: 'ferpereira22@gmail.com',
     pass: 'cplm ibga bvee edhd'
-  }
+  },
+  tls: {
+    rejectUnauthorized: false
+}
 })
 
 app.get('/mail', async (req, res) => {
-  let result = await transport.sendMail({
+  let result = await transporter.sendMail({
     from: 'ferpereira22@gmail.com',
     to: 'ferpereira22@hotmail.com',
-    subject: 'Hola',
+    subject: 'Usted fue fernandeado',
     html: `<div>
             <h1>Usted fue fernandeado</h1>
-            <img src="cid:ferpe">
+            <img src="cid:fer"/>
           </div>`,
-    attachments: [{
-      filename: 'fer.jpg',
-      path: __dirname+'src/fer.jpg',
-      cid: 'ferpe'
-    }]
+    attachments: [
+      {
+        filename: 'fer.jpg',
+        path: './src/fer.jpg',
+        cid: 'fer'
+      },
+      {
+        filename: 'CV Fernando Pereira 2022.pdf',
+        path: './src/CV Fernando Pereira 2022.pdf',
+      },
+      {
+        filename: 'Europa 2022.xlsx',
+        path: './src/Europa 2022.xlsx',
+      }
+
+    ]
   })
 
   res.send({ status: 'sucess', result: 'Email sent' })
-})
-
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000')
 })
